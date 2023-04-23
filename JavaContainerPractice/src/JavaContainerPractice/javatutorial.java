ee1a8e1b888da9c4be577b464be3ad0590fb041b
dell <dell@DESKTOP-T7RG3RQ>
2023-04-23 10:29:15 +0530

    This my second commit

---
+++ JavaContainerPractice/src/JavaContainerPractice/javatutorial.java
@@ -0,0 +1,16 @@
+
+package JavaContainerPractice;
+
+import java.io.IOException;
+
+
+
+public class javatutorial {
+	
+	
+	public static void main(String args[]) throws IOException
+	{
+		System.out.println("Hi I am learning git and git hub");
+	}
+
+}
